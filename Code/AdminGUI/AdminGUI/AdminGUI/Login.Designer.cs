86a8fc54180d3901e44a08eb498d797f5457ee13
Alex Martin <example@test.com>
2019-11-07 23:01:06 +0000

    20191107 GUI for admin. Log on page created - will always return true. User control is out of scope for this production.

---
+++ Code/AdminGUI/AdminGUI/AdminGUI/Login.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AdminGUI
+{
+   partial class Login
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.txtUserName = new System.Windows.Forms.TextBox();
+         this.txtPassword = new System.Windows.Forms.TextBox();
+         this.btnSubmit = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // txtUserName
+         // 
+         this.txtUserName.Location = new System.Drawing.Point(12, 12);
+         this.txtUserName.Name = "txtUserName";
+         this.txtUserName.Size = new System.Drawing.Size(144, 20);
+         this.txtUserName.TabIndex = 0;
+         this.txtUserName.Text = "User Name";
+         // 
+         // txtPassword
+         // 
+         this.txtPassword.Location = new System.Drawing.Point(12, 38);
+         this.txtPassword.Name = "txtPassword";
+         this.txtPassword.PasswordChar = '*';
+         this.txtPassword.Size = new System.Drawing.Size(144, 20);
+         this.txtPassword.TabIndex = 1;
+         // 
+         // btnSubmit
+         // 
+         this.btnSubmit.Location = new System.Drawing.Point(13, 65);
+         this.btnSubmit.Name = "btnSubmit";
+         this.btnSubmit.Size = new System.Drawing.Size(143, 23);
+         this.btnSubmit.TabIndex = 2;
+         this.btnSubmit.Text = "Submit";
+         this.btnSubmit.UseVisualStyleBackColor = true;
+         this.btnSubmit.Click += new System.EventHandler(this.Submit_Click);
+         // 
+         // Login
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.AutoSize = true;
+         this.ClientSize = new System.Drawing.Size(168, 106);
+         this.Controls.Add(this.btnSubmit);
+         this.Controls.Add(this.txtPassword);
+         this.Controls.Add(this.txtUserName);
+         this.Name = "Login";
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         this.Text = "Login";
+         this.Load += new System.EventHandler(this.Login_Load);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TextBox txtUserName;
+      private System.Windows.Forms.TextBox txtPassword;
+      private System.Windows.Forms.Button btnSubmit;
+   }
+}
+
---
+++ Code/AdminGUI/AdminGUI/AdminGUI/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminGUI
+{
+   public partial class Login : Form
+   {
+      public Login()
+      {
+         InitializeComponent();
+      }
+
+      private void Login_Load(object sender, EventArgs e)
+      {
+         
+      }
+
+      private void Submit_Click(object sender, EventArgs e)
+      {
+         this.Hide();
+         AdminView adminView = new AdminView();
+         adminView.Closed += (s, args) => this.Close();
+         adminView.Show();
+      }
+   }
+}

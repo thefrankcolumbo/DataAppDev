702d9e8449219bd2367962feb0cc663c10c61963
Alex Martin <example@test.com>
2019-11-11 15:42:32 +0000

    20191111 ADMIN GUI - adding comments to public methods and /// documentation for Controller class

--- Code/AdminGUI/AdminGUI/AdminGUI/Controller.cs
+++ Code/AdminGUI/AdminGUI/AdminGUI/Controller.cs
@@ -16,14 +16,125 @@ namespace AdminGUI
       {
          setupConnection();
       }
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      /////////////////////////////// THIS DOESNT WORK ATM //////////////////////////////////////////////////////////////////////
       public string searchForProductByPartialId(string partialId)
       {
          return searchForProducts(partialId, "SearchForProductByIdLikness", "@id");
       }
+      /////////////////////////////// THIS DOESNT WORK ATM //////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      /// <summary>
+      /// Returns a string of zero to many products, dependant on the search results.
+      /// A serch will be carried out using a wildcard to the right of the string.
+      /// example:
+      /// "ABC" would return "ABC", "ABCD" and "abcde"
+      /// "ABC" would not return "AABC"or "babc"
+      /// </summary>
+      /// <param name="partialName"></param>
+      /// <returns>string
+      /// String construction: 
+      /// each product is seperated by a colon. 
+      /// each field of a product is seperated by a comma. 
+      /// Fields are: Name, Id. 
+      /// </returns>
       public string searchForProductByPartialName(string partialName)
       {
          return searchForProducts(partialName, "SearchForProductByNameLikness", "@name");
       }
+      
+      public void updateProductForSaleById(string[] productDetails)
+      {
+         openConnection();
+         SqlCommand cmd = new SqlCommand("EditProduct", conn);
+         cmd.CommandType = CommandType.StoredProcedure;
+         cmd.Parameters.AddWithValue("@CatName", productDetails[0]);
+         cmd.Parameters.AddWithValue("@ProdName", productDetails[1]);
+         cmd.Parameters.AddWithValue("@OnSale", productDetails[2]);
+         cmd.Parameters.AddWithValue("@ProdDescription", productDetails[3]);
+         cmd.Parameters.AddWithValue("@ProdQuantity", productDetails[4]);
+         cmd.Parameters.AddWithValue("@MinStock", productDetails[5]);
+         cmd.Parameters.AddWithValue("@PictFilePath", productDetails[6]);
+         cmd.Parameters.AddWithValue("@RetPrice", productDetails[7]);
+         cmd.Parameters.AddWithValue("@id", productDetails[8]);
+         cmd.ExecuteReader();
+         closeConnection();
+      }
+      /// <summary>
+      /// Method to add a new product to stock.
+      /// </summary>
+      /// <param name="productDetails">Array 
+      /// element[0] Category. 
+      /// element[1] Product Name. 
+      /// element[2] OnSale - 1 for yes, 2 for no. 
+      /// element[3] Product description - upto 254 characters. 
+      /// element[4] Product Quantity - must be numbers only. 
+      /// element[5] Minimum Stock - must be numbers only. 
+      /// element[6] File path for the product picture. 
+      /// element[7] Retail price - must be in a currency format e.g. 1.99. 
+      /// </param>
+      public void AddNewProductForSale(string[] productDetails)
+      {
+         openConnection();
+         SqlCommand cmd = new SqlCommand("AddNewProductAllDetails", conn);
+         cmd.CommandType = CommandType.StoredProcedure;
+         cmd.Parameters.AddWithValue("@CatName", productDetails[0]);
+         cmd.Parameters.AddWithValue("@ProdName", productDetails[1]);
+         cmd.Parameters.AddWithValue("@OnSale", productDetails[2]);
+         cmd.Parameters.AddWithValue("@ProdDescription", productDetails[3]);
+         cmd.Parameters.AddWithValue("@ProdQuantity", productDetails[4]);
+         cmd.Parameters.AddWithValue("@MinStock", productDetails[5]);
+         cmd.Parameters.AddWithValue("@PictFilePath", productDetails[6]);
+         cmd.Parameters.AddWithValue("@RetPrice", productDetails[7]);
+         cmd.ExecuteReader();
+         closeConnection();
+      }
+
+      /// <summary>
+      /// Returns a String of all products that are NOT for sale.
+      /// </summary>
+      /// <returns>string
+      /// String construction: 
+      /// each product is seperated by a colon. 
+      /// each field of a product is seperated by a comma. 
+      /// Fields are: Name, ForSale, Description, Quantity, MinimumStock, NumberSold, PictureFilePath, RetailPrice, CategoryName. 
+      /// </returns>
+      public string viewStockNotForSale()
+      {
+         return getDataFromView("ViewAllProductsNotForSale");
+      }
+
+      /// <summary>
+      /// Returns a String of all products that are for sale.
+      /// </summary>
+      /// <returns>string
+      /// String construction: 
+      /// each product is seperated by a colon. 
+      /// each field of a product is seperated by a comma. 
+      /// Fields are: Name, ForSale, Description, Quantity, MinimumStock, NumberSold, PictureFilePath, RetailPrice, CategoryName. 
+      /// </returns>
+      public string viewStockForSale()
+      {
+         return getDataFromView("ViewAllProductsForSale");
+      }
+
+      /// <summary>
+      /// Returns a string of all products which have the current stock levels below thier minimum stock levels
+      /// </summary>
+      /// <returns>string
+      /// String construction: 
+      /// each product is seperated by a colon. 
+      /// each field of a product is seperated by a comma. 
+      /// Fields are: Name, Quantity, MinimumStock. 
+      /// </returns>
+      public string CurrentStockUnderMinimumOrderAmount()
+      {
+         return getDataFromView("ViewAllProductsBelowMinimumStockLevels");
+      }
+
+
       private string searchForProducts(string partialString, string storedProcedure, string field)
       {
          string Output = "";
@@ -44,51 +155,6 @@ namespace AdminGUI
          closeConnection();
          if (Output != "") Output = Output.Substring(0, Output.Length - 1);
          return Output;
-      }
-      public void updateProductForSaleById(string[] productDetails)
-      {
-         openConnection();
-         SqlCommand cmd = new SqlCommand("EditProduct", conn);
-         cmd.CommandType = CommandType.StoredProcedure;
-         cmd.Parameters.AddWithValue("@CatName", productDetails[0]);
-         cmd.Parameters.AddWithValue("@ProdName", productDetails[1]);
-         cmd.Parameters.AddWithValue("@OnSale", productDetails[2]);
-         cmd.Parameters.AddWithValue("@ProdDescription", productDetails[3]);
-         cmd.Parameters.AddWithValue("@ProdQuantity", productDetails[4]);
-         cmd.Parameters.AddWithValue("@MinStock", productDetails[5]);
-         cmd.Parameters.AddWithValue("@PictFilePath", productDetails[6]);
-         cmd.Parameters.AddWithValue("@RetPrice", productDetails[7]);
-         cmd.Parameters.AddWithValue("@id", productDetails[8]);
-         cmd.ExecuteReader();
-         closeConnection();
-      }
-      public void AddNewProductForSale(String[] productDetails)
-      {
-         openConnection();
-         SqlCommand cmd = new SqlCommand("AddNewProductAllDetails", conn);
-         cmd.CommandType = CommandType.StoredProcedure;
-         cmd.Parameters.AddWithValue("@CatName", productDetails[0]);
-         cmd.Parameters.AddWithValue("@ProdName", productDetails[1]);
-         cmd.Parameters.AddWithValue("@OnSale", productDetails[2]);
-         cmd.Parameters.AddWithValue("@ProdDescription", productDetails[3]);
-         cmd.Parameters.AddWithValue("@ProdQuantity", productDetails[4]);
-         cmd.Parameters.AddWithValue("@MinStock", productDetails[5]);
-         cmd.Parameters.AddWithValue("@PictFilePath", productDetails[6]);
-         cmd.Parameters.AddWithValue("@RetPrice", productDetails[7]);
-         cmd.ExecuteReader();
-         closeConnection();
-      }
-      public string viewStockNotForSale()
-      {
-         return getDataFromView("ViewAllProductsNotForSale");
-      }
-      public string viewStockForSale()
-      {
-         return getDataFromView("ViewAllProductsForSale");
-      }
-      public string CurrentStockUnderMinimumOrderAmount()
-      {
-         return getDataFromView("ViewAllProductsBelowMinimumStockLevels");
       }
       private string getDataFromView(string view)
       {
